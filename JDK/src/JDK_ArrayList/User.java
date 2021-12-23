3748fbd1efda366492616b2bbe17c0143f8d7767
yyangbobo <1360664142@qq.com>
2021-12-23 22:29:09 +0800

    map

---
+++ JDK/src/JDK_ArrayList/User.java
@@ -0,0 +1,24 @@
+package JDK_ArrayList;
+
+public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+
+}
